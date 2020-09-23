0422bee164a71501db23945052edf943f7265c3c
Nikola Klipa <klipa.nikola@gmail.com>
2020-09-23 19:10:31 +0200

    Change query to include all proxies from Defisaver automation

--- src/07_user_proxy.sql
+++ src/07_user_proxy.sql
@@ -5,17 +5,17 @@ CREATE TEMPORARY TABLE all_txes AS (
     SELECT transaction_hash
     FROM wrappers_query
   )
-  SELECT from_address, to_address, `hash`
-       FROM   `bigquery-public-data.crypto_ethereum.transactions`
-       WHERE (`hash` IN (SELECT transaction_hash FROM hashes))
+  SELECT from_address, to_address, transaction_hash
+       FROM   `bigquery-public-data.crypto_ethereum.traces`
+       -- take all transactions that used Uniswap and in their trace DSProxys execute is called
+       WHERE (transaction_hash IN (SELECT transaction_hash FROM hashes)) AND input LIKE '0x1cff79cd%')
 );
 
 CREATE TABLE user_proxy AS (
   WITH cte AS (
     SELECT 
-        from_address as user,
         to_address as proxy,
-        `hash` as transaction_hash,
+        transaction_hash,
         ROW_NUMBER() OVER (
             PARTITION BY 
                 to_address
