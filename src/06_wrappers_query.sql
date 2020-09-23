77f2b7aafba517d04d67b786129a7d10dc943d7a
Nikola Klipa <klipa.nikola@gmail.com>
2020-09-23 20:58:09 +0200

    Take also eth transactions

--- src/06_wrappers_query.sql
+++ src/06_wrappers_query.sql
@@ -32,11 +32,27 @@ CREATE TABLE wrappers_query AS (
         )
         AND from_address IN ('0x0aa70981311d60a9521c99cecfdd68c3e5a83b83', '0x1e30124fde14533231216d95f7798cd0061e5cf8', '0x880a845a85f843a5c67db2061623c6fc3bb4c511', '0xff92ada50cdc8009686867b4a470c8769bedb22d')
         AND block_timestamp < @cutoff_timestamp
+  ),
+  uniswap_traces as (
+    SELECT
+        from_address as address
+    FROM `bigquery-public-data.crypto_ethereum.traces`
+    WHERE
+      to_address IN (
+         SELECT contract
+         FROM uniswap_contracts
+      ) AND from_address IN ('0x0aa70981311d60a9521c99cecfdd68c3e5a83b83', '0x1e30124fde14533231216d95f7798cd0061e5cf8', '0x880a845a85f843a5c67db2061623c6fc3bb4c511', '0xff92ada50cdc8009686867b4a470c8769bedb22d')
+      AND block_timestamp < @cutoff_timestamp
+      AND call_type = 'call'
   )
   SELECT *
   FROM (
-      SELECT *
+      SELECT to_address, from_address, transaction_hash
       FROM token_transfer_senders
+      UNION
+      ALL
+      SELECT to_address, from_address, transaction_hash
+      FROM uniswap_traces
     )
   WHERE from_address NOT IN (
       SELECT contract
